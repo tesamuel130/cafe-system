cb7d655e19235af80a69a0338ca785da243cf434
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-04-19 10:29:20 -0900

    com

--- Project/FrontEnd/src/js/addToCart.js
+++ Project/FrontEnd/src/js/addToCart.js
@@ -1,18 +1,18 @@
-function AddCart(){
-    document.querySelector('.trycl').addEventListener('click', ()=>{
-        document.querySelector('.carts').innerHTML = `
-            <div class="cart-card-list">
-                <div class="cart-card">
-                    <img src="/Project/FrontEnd/src/css/img/appetizer.png" alt="food image">
-                    <div class="ord-dit">
-                        <h1>appetizer</h1>
-                        <p>price: 20$</p>
-                    </div>
-                </div>
-            </div>
-        `
-    })
+// function AddCart(){
+//     // document.querySelector('.trycl').addEventListener('click', ()=>{
+//     //     document.querySelector('.carts').innerHTML = `
+//     //         <div class="cart-card-list">
+//     //             <div class="cart-card">
+//     //                 <img src="/Project/FrontEnd/src/css/img/appetizer.png" alt="food image">
+//     //                 <div class="ord-dit">
+//     //                     <h1>appetizer</h1>
+//     //                     <p>price: 20$</p>
+//     //                 </div>
+//     //             </div>
+//     //         </div>
+//     //     `
+//     })
 
-}
+// }
 
-export default AddCart;
+// export default AddCart;
