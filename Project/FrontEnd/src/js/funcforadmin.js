d409cb87459c9a18293e46e9c3110ce63a184a04
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-05-15 03:19:31 -0900

    edit some add food functionality

--- Project/FrontEnd/src/js/funcforadmin.js
+++ Project/FrontEnd/src/js/funcforadmin.js
@@ -1,2 +1,18 @@
 // functionality for the order admin page
-const confirm = document.querySelector(".");
+const addedFoodList = document.querySelector(".edit-food-container");
+const editfoodbtn = document.querySelectorAll(".edit-food");
+const closeEditFoodBtn = document.getElementById("editFoodClose");
+
+const openFoodEdit = () => {
+  addedFoodList.classList.toggle("open-edit-food");
+};
+
+const closeFoodEdit = () => {
+  addedFoodList.classList.remove("open-edit-food");
+};
+
+editfoodbtn.forEach((button, index) => {
+  button.addEventListener("click", openFoodEdit);
+});
+
+closeEditFoodBtn.addEventListener("click", closeFoodEdit);
